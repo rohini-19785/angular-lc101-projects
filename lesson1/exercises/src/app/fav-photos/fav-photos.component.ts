2ec7d1d63df3a1157d9ee25aa4145d9049abb9bf
rohini-19785 <58015099+rohini-19785@users.noreply.github.com>
2019-12-18 12:43:23 -0600

    Made changes as per 28.7.2. Part 1: Modify the CSS

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'My Favorite Photos';
   image1 = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://images.pexels.com/photos/462118/pexels-photo-462118.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500';
+  image3 = 'https://www.goodnewsnetwork.org/wp-content/uploads/2019/09/Squirrel-Hugging-Flower-Feature-SWNS.jpg';
 
   constructor() { }
 
